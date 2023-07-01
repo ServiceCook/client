49bb5afc37155b9d8f6623a5343e912779de55ef
Solideo Putra Zendrato <solidzendrato@gmail.com>
2023-07-01 17:31:52 +0200

    modified service card

--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -5,7 +5,7 @@ function ServiceCard({ speciality, place, picture, _id, owner, pricePerPerson}) 
         <div className="service-card-box">
             <h1>{speciality}</h1>
             {picture ? <img src={picture} alt="img" className="image-service"/> : <p>sorry, no picture</p>}
-            <h4>Price {pricePerPerson} €</h4>
+            <h4>Price Per Person {pricePerPerson} €</h4>
             <h3>Service by {owner.name}</h3>
             <h2>{place}</h2>
             <Link to={{ pathname: `/services/${_id}`, state: { picture } }}>
