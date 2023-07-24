ad055a816e80598df57a06aff86285e703741249
Dok <pierredok@outlook.fr>
2023-07-24 13:25:08 +0200

    Burger menu mobile done need adjust priority of vision

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,19 @@
 import { useContext, useState } from "react";
 import { AuthContext } from "../context/auth.context";
 import { Link } from "react-router-dom";
+import classNames from "classnames";
 import logo from '../images/logo.svg'
 function NavBar() {
 
-  const  [isLoggedIn, logOutUser] = useContext(AuthContext);
+  const  { isLoggedIn, logOutUser} = useContext(AuthContext);
   const [showLink, setShowLink] = useState(false)
 
  const handleShowLinks = () => {
     setShowLink(!showLink);
+  }
+
+  const closeMenu = () => {
+    setShowLink(false);
   }
   console.log(showLink);
   return (
@@ -65,24 +70,24 @@ function NavBar() {
       
       </nav> */}
 
-      <nav className={`${showLink ? "showNav" : "hide-nav"}`}>
-        <div>
+      <nav>
+        <div className={`${showLink ? "showNav" : "hide-nav"}`}>
           <ul>
-            <Link to='/'>
+            <Link to='/' onClick={closeMenu}>
               <img className="logo" src={logo} alt="logo"/>
             </Link>
 
-            <Link to="/services">
+            <Link to="/services" onClick={closeMenu}>
               <li>Our chefs</li>
             </Link>
 
 
             {isLoggedIn && (
             <> 
-                <Link to="/profile">
+                <Link to="/profile" onClick={closeMenu}>
                   <li>Profile</li>
                 </Link>
-                <Link to="/message">
+                <Link to="/message" onClick={closeMenu}>
                   <li>Contact Us</li>
                 </Link>
 
@@ -98,17 +103,17 @@ function NavBar() {
             {!isLoggedIn && (
             <div className="navbarAuth navbar">
             
-              <Link to="/signup">
+              <Link to="/signup" onClick={closeMenu}>
                 <li>Sign Up</li>
               </Link>
-              <Link to="/login">
+              <Link to="/login" onClick={closeMenu}>
                 <li>Log In</li>
               </Link>
             </div>
             )}
           </ul>
         </div>
-        <button className="navbar-burger" onClick={handleShowLinks}>
+        <button className={classNames ("navbar-burger", {'open': showLink})} onClick={handleShowLinks}>
           <span className="burger-bar"></span>
         </button>
       </nav>
