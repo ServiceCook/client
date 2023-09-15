dc6b7cbebc2c6dd224fe07cf6119102f3c524c92
Pierre Dok <pierredok@outlook.fr>
2023-09-15 02:37:25 +0200

    commentary animation because to much bug

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,77 +6,77 @@ import { useEffect } from 'react';
 
 function HomePage() {
 
-  // Function to check if an element is in the viewport
-function isElementInViewport(el) {
-  const rect = el.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-}
+//   // Function to check if an element is in the viewport
+// function isElementInViewport(el) {
+//   const rect = el.getBoundingClientRect();
+//   return (
+//     rect.top >= 0 &&
+//     rect.left >= 0 &&
+//     rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+//     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+//   );
+// }
 
-// Function to add animations when elements are in the viewport
-function handleScrollAnimations() {
-  const elementsToAnimate = document.querySelectorAll('.logo-home');
-  const imgDescriptionElement = document.querySelectorAll('.img-description');
-  const whoAreWeElement = document.querySelectorAll('.who-are-we-text');
-  const descriptionWhoAreWeElement = document.querySelectorAll('.description-who-are-we');
-  const lookAllChefElement = document.querySelectorAll('.look-all-chef-text');
+// // Function to add animations when elements are in the viewport
+// function handleScrollAnimations() {
+//   const elementsToAnimate = document.querySelectorAll('.logo-home');
+//   const imgDescriptionElement = document.querySelectorAll('.img-description');
+//   const whoAreWeElement = document.querySelectorAll('.who-are-we-text');
+//   const descriptionWhoAreWeElement = document.querySelectorAll('.description-who-are-we');
+//   const lookAllChefElement = document.querySelectorAll('.look-all-chef-text');
 
-  elementsToAnimate.forEach((element) => {
-    const isInViewport = isElementInViewport(element);
-    if (isInViewport) {
-      element.classList.add('animate');
-    } else {
-      element.classList.remove('animate');
-    }
-  });
+//   elementsToAnimate.forEach((element) => {
+//     const isInViewport = isElementInViewport(element);
+//     if (isInViewport) {
+//       element.classList.add('animate');
+//     } else {
+//       element.classList.remove('animate');
+//     }
+//   });
 
-  imgDescriptionElement.forEach(element => {
-    const isInViewport = isElementInViewport(element);
-    if(isInViewport) {
-      element.classList.add("animate")
-    } else {
-      element.classList.remove("animate")
-    }
-  });
+//   imgDescriptionElement.forEach(element => {
+//     const isInViewport = isElementInViewport(element);
+//     if(isInViewport) {
+//       element.classList.add("animate")
+//     } else {
+//       element.classList.remove("animate")
+//     }
+//   });
 
-  whoAreWeElement.forEach(element => {
-    const isInViewport = isElementInViewport(element);
-    if(isInViewport) {
-      element.classList.add("animate")
-    } else {
-      element.classList.remove("animate")
-    }
-  });
+//   whoAreWeElement.forEach(element => {
+//     const isInViewport = isElementInViewport(element);
+//     if(isInViewport) {
+//       element.classList.add("animate")
+//     } else {
+//       element.classList.remove("animate")
+//     }
+//   });
 
-  descriptionWhoAreWeElement.forEach(element => {
-    const isInViewport = isElementInViewport(element);
-    if(isInViewport) {
-      element.classList.add("animate")
-    } else {
-      element.classList.remove("animate")
-    }
-  });
+//   descriptionWhoAreWeElement.forEach(element => {
+//     const isInViewport = isElementInViewport(element);
+//     if(isInViewport) {
+//       element.classList.add("animate")
+//     } else {
+//       element.classList.remove("animate")
+//     }
+//   });
 
-  lookAllChefElement.forEach(element => {
-    const isInViewport = isElementInViewport(element);
-    if(isInViewport) {
-      element.classList.add("animate")
-    } else {
-      element.classList.remove("animate")
-    }
-  });
+//   lookAllChefElement.forEach(element => {
+//     const isInViewport = isElementInViewport(element);
+//     if(isInViewport) {
+//       element.classList.add("animate")
+//     } else {
+//       element.classList.remove("animate")
+//     }
+//   });
 
-};
+// };
 
-useEffect(() => {
-  document.addEventListener('scroll', handleScrollAnimations);
+// useEffect(() => {
+//   document.addEventListener('scroll', handleScrollAnimations);
   
-  handleScrollAnimations();
-}, [])
+//   handleScrollAnimations();
+// }, [])
 
 
     return (
