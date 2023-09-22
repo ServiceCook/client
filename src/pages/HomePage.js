7220fbe523719bf1dc6034394b1a52490423f6d3
Dok <pierredok@outlook.fr>
2023-09-22 18:46:12 +0200

    Fix animation problem

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,82 +2,8 @@ import { Link } from 'react-router-dom';
 import descriptionImg from '../images/Homepage.png'
 import pasta from '../images/Pasta-copy.jpg'
 import logo from '../images/logo.svg'
-import { useEffect } from 'react';
 
 function HomePage() {
-
-//   // Function to check if an element is in the viewport
-// function isElementInViewport(el) {
-//   const rect = el.getBoundingClientRect();
-//   return (
-//     rect.top >= 0 &&
-//     rect.left >= 0 &&
-//     rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-//     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-//   );
-// }
-
-// // Function to add animations when elements are in the viewport
-// function handleScrollAnimations() {
-//   const elementsToAnimate = document.querySelectorAll('.logo-home');
-//   const imgDescriptionElement = document.querySelectorAll('.img-description');
-//   const whoAreWeElement = document.querySelectorAll('.who-are-we-text');
-//   const descriptionWhoAreWeElement = document.querySelectorAll('.description-who-are-we');
-//   const lookAllChefElement = document.querySelectorAll('.look-all-chef-text');
-
-//   elementsToAnimate.forEach((element) => {
-//     const isInViewport = isElementInViewport(element);
-//     if (isInViewport) {
-//       element.classList.add('animate');
-//     } else {
-//       element.classList.remove('animate');
-//     }
-//   });
-
-//   imgDescriptionElement.forEach(element => {
-//     const isInViewport = isElementInViewport(element);
-//     if(isInViewport) {
-//       element.classList.add("animate")
-//     } else {
-//       element.classList.remove("animate")
-//     }
-//   });
-
-//   whoAreWeElement.forEach(element => {
-//     const isInViewport = isElementInViewport(element);
-//     if(isInViewport) {
-//       element.classList.add("animate")
-//     } else {
-//       element.classList.remove("animate")
-//     }
-//   });
-
-//   descriptionWhoAreWeElement.forEach(element => {
-//     const isInViewport = isElementInViewport(element);
-//     if(isInViewport) {
-//       element.classList.add("animate")
-//     } else {
-//       element.classList.remove("animate")
-//     }
-//   });
-
-//   lookAllChefElement.forEach(element => {
-//     const isInViewport = isElementInViewport(element);
-//     if(isInViewport) {
-//       element.classList.add("animate")
-//     } else {
-//       element.classList.remove("animate")
-//     }
-//   });
-
-// };
-
-// useEffect(() => {
-//   document.addEventListener('scroll', handleScrollAnimations);
-  
-//   handleScrollAnimations();
-// }, [])
-
 
     return (
       <div className="home-page">
